aac341f20640adc7c3170d8a1584f7dd2cb1f300
Aisha Mohammad <aisha.mohammad@booking.com>
2026-02-27 11:10:01 +0000

    add status enum, getter and setter

--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -13,6 +13,7 @@ public class TicketTest {
         assertEquals("test", testTicket.getTitle());
         assertEquals("test 1", testTicket.getDescription());
         assertEquals("low-level", Ticket.Priority.LOW.getPriority());
+        assertEquals("Open", Ticket.Status.OPEN.getStatus());
 
     }
 
